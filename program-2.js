c0cdf2a4782a2d37ad91865f56823b23dde2379b
PrinceDev9 <bavishiprince90@gmail.com>
2025-06-16 11:21:20 +0530

    program - 2 complete

--- program-2.js
+++ program-2.js
@@ -2,3 +2,27 @@
 
 // Sample Input: [1, 2, [3, 4], [5, [6, 7]]];
 // Expected Output: [1, 2, 3, 4, 5, 6, 7];
+
+const complexArray = [1, 2, 3, [4, 6, [10, 34, 67, 89]]];
+let flatAry = [];
+
+function callForIterate(ary) {
+  for (let i = 0; i < ary.length; i++) {
+    if (Array.isArray(ary[i])) {
+      callForIterate(ary[i]); // Recursively flatten
+    } else {
+      flatAry.push(ary[i]); // Push non-array element
+    }
+  }
+}
+
+callForIterate(complexArray);
+
+console.log("flat ary:", flatAry);
+
+
+// using the built in function 
+const complexArray1 = [1, 2, 3, [4, 6, [10, 34, 67, 89]]];
+const flatAry1 = complexArray1.flat(Infinity);
+
+console.log("flat ary:", flatAry1);
