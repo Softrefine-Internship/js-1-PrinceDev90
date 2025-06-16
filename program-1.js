0e23d85f45be0a553cdc30ca531689ea91e9d55b
PrinceDev9 <bavishiprince90@gmail.com>
2025-06-16 11:17:46 +0530

    program - 1 is complete

--- program-1.js
+++ program-1.js
@@ -8,11 +8,30 @@ const case1 = sample.map((ele, i) => {
   return ele;
 });
 
-
 // case - 2
-const case2 = [...sample];
+const case2 = [...sample]; // not for sub array
 
 // case - 3
-const case3 = sample;
+const case3 = sample; //pass the reference 
 
-console.log();
+
+// dynamic program for cloning the array
+
+const complexArray = [1, 2, 3, [4, 6, [10, 34, 67, 89]]];
+let flatAry = [];
+
+function callForIterate(ary) {
+
+  for (let i = 0; i < ary.length; i++) {
+
+    if (Array.isArray(ary[i])) {
+      callForIterate(ary[i]); // Recursively flatten
+    } else {
+      flatAry.push(ary[i]); // Push non-array element
+    }
+  }
+}
+
+callForIterate(complexArray);
+
+console.log("flat ary:", flatAry);
