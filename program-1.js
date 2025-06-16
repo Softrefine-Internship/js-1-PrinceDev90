4b3bc5e041130367836b1076ba2599b96a20aaff
PrinceDev9 <bavishiprince90@gmail.com>
2025-06-16 10:41:00 +0530

    date:16/06/2025 time:10:40 AM day - 1 start commit

--- program-1.js
+++ program-1.js
@@ -1,1 +1,18 @@
 // Write a JavaScript program to clone an array.
+"use strict";
+
+const sample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// case - 1
+const case1 = sample.map((ele, i) => {
+  return ele;
+});
+
+
+// case - 2
+const case2 = [...sample];
+
+// case - 3
+const case3 = sample;
+
+console.log();
